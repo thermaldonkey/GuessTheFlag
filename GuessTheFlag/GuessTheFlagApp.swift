1bbeda0d94234e807abe8a7e671e8f4944bf5a8f
thermaldeveloper <thermaldeveloper@icloud.com>
2025-03-25 10:32:43 -0500

    Initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Brad Rice on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
